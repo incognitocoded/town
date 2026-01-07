978a6de5ff5275d023fecb4f1455be0ae8c7d5fc
incognitocoded <nazarperec18@gmail.com>
2026-01-07 20:06:39 +0200

    Refactor loader to improve error handling
    Updated loader to use pcall for error handling and added warning for connection issues.

--- loader.lua
+++ loader.lua
@@ -1,1 +1,16 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/incognitocoded/town/main/loader.lua"))()
+-- [[ LOST HUB OFFICIAL LOADER ]] --
+-- [[ TG/@losthubscript ]] --
+
+-- Замени эту ссылку на свою RAW ссылку с GitHub
+local my_github_url = "https://raw.githubusercontent.com/incognitocoded/town/main/loader.lua"
+
+local success, content = pcall(function()
+    return game:HttpGet(my_github_url)
+end)
+
+if success then
+    loadstring(content)()
+else
+    -- Если ссылка битая или нет интернета, выдаст ошибку в консоль F9
+    warn("LOST HUB: Error connecting to GitHub!")
+end
