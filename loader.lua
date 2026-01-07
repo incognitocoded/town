7ce58b2be2939f70544c82b5d03a8cc834d713dc
incognitocoded <nazarperec18@gmail.com>
2026-01-07 20:03:51 +0200

    Refactor loader to simplify script loading

--- loader.lua
+++ loader.lua
@@ -1,17 +1,1 @@
--- [[ LOST HUB LOADER V1 ]] --
--- [[ TG/@losthubscript ]] --
-
-local raw_url = "https://raw.githubusercontent.com/incognitocoded/town/main/loader.lua" 
-
--- Проверка и запуск
-local success, result = pcall(function()
-    return game:HttpGet(raw_url)
-end)
-
-if success then
-    -- Логика блокировки меню при движении слайдера
-    -- (Эта часть уже встроена в основной скрипт V1, который ты залил на GitHub)
-    loadstring(result)()
-else
-    print("Error loading script from GitHub")
-end
+loadstring(game:HttpGet("https://raw.githubusercontent.com/incognitocoded/town/main/loader.lua"))()
