519c38eec3884b7bf5a20b94f526bbed48561a2a
incognitocoded <nazarperec18@gmail.com>
2026-01-07 20:09:59 +0200

    Change GitHub URL and enhance error messages
    Updated GitHub URL and improved error handling.

--- loader.lua
+++ loader.lua
@@ -1,13 +1,12 @@
--- Замени ссылку ниже на ту, которую ты скопировал через кнопку Raw
-local raw_url = "https://raw.githubusercontent.com/incognitocoded/town/main/loader.lua" 
+local my_github_url = "https://raw.githubusercontent.com/incognitocoded/town/main/main.lua" 
 
-local success, result = pcall(function()
-    return game:HttpGet(raw_url)
+local success, content = pcall(function()
+    return game:HttpGet(my_github_url)
 end)
 
 if success then
-    print("LOST HUB: Скрипт успешно загружен!")
-    loadstring(result)() -- Это запустит твой GUI из облака
+    loadstring(content)()
 else
-    warn("Ошибка: Не удалось получить код с GitHub. Проверь ссылку!")
+    -- Если ссылка битая или нет интернета, выдаст ошибку в консоль F9
+    warn("LOST HUB: Error connecting to GitHub!")
 end
